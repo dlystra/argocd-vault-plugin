d8e3cdd33881a26eb2c704e78f2c09e95d842a4d
dlystra <dlystra@redhat.com>
2025-11-17 12:06:44 -0600

    updated secrets

--- vault/create-secrets.sh
+++ vault/create-secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-oc exec -it vault-0 -n vault -- vault kv put secret/demo helm="thisisthehelmsecret"
-oc exec -it vault-0 -n vault -- vault kv put secret/demo values="thisisthevaluessecret"
-oc exec -it vault-0 -n vault -- vault kv put secret/demo kustomize="thisisthekustomizesecret"
-oc exec -it vault-0 -n vault -- vault kv put secret/demo patch="thisisthepatchsecret"
+oc exec -it vault-0 -n vault -- vault kv put secret/helm demo="thisisthehelmsecret"; \
+oc exec -it vault-0 -n vault -- vault kv put secret/values demo="thisisthevaluessecret"; \
+oc exec -it vault-0 -n vault -- vault kv put secret/kustomize demo="thisisthekustomizesecret"; \
+oc exec -it vault-0 -n vault -- vault kv put secret/patch demo="thisisthepatchsecret"
